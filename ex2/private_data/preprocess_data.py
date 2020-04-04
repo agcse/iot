16bcfab2e7e11aad8d8d951cc7f36fc230905cf8
andrey-golubev <andpgolubev@gmail.com>
2020-04-04 20:45:04 +0300

    Add conversion to relative coordinates of sensors

--- ex2/private_data/preprocess_data.py
+++ ex2/private_data/preprocess_data.py
@@ -13,11 +13,31 @@ if __name__ == '__main__':
     # data.drop(['co2', 'humidity', 'light', 'temperature'], axis=1, inplace=True)
     # data.to_csv(abspath('data.csv'))
 
+
     # locations.csv:
     # locations = pd.read_csv(abspath('locations.csv'), sep=';')
     # locations = locations[['id', 'locations', 'latitude', 'longitude']]
     # locations['id'] = locations['id'].map(
     #     lambda idstr: '-'.join(a + b for a,b in zip(idstr[::2], idstr[1::2])).lower())
     # locations.to_csv(abspath('locations.csv'))
+
+
+    # 2nd part of locations.csv preprocessing:
+    # corners = []
+    # with open(abspath('map_corners.csv'), 'r') as corners_file:
+    #     line = corners_file.readline()  # skip first line
+    #     line = corners_file.readline()
+    #     corners = [float(e) for e in line.split(',')]
+    # latitudes = corners[::2]
+    # longitudes = corners[1::2]
+    # min_latitude, max_latitude = min(latitudes), max(latitudes)
+    # min_longitude, max_longitude = min(longitudes), max(longitudes)
+
+    # locations = pd.read_csv(abspath('locations.csv'), sep=',')
+    # locations['latitude'] = locations['latitude'].map(
+    #     lambda x: (x - min_latitude) / (max_latitude - min_latitude))
+    # locations['longitude'] = locations['longitude'].map(
+    #     lambda x: (x - min_longitude) / (max_longitude - min_longitude))
+    # locations.to_csv(abspath('relative_locations.csv'))
 
     pass
